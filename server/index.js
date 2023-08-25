88958c8c014e31c54ac28b5dcb398c3b0c76b251
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-08-25 22:12:13 +0300

    fixing some bugs

--- server/index.js
+++ server/index.js
@@ -1,45 +1,67 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import {graphqlHTTP} from 'express-graphql'
-import {buildSchema} from 'graphql'
-dotenv.config()
-const PORT = process.env.PORT | 5000
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
-const app = express()
+dotenv.config();
+const PORT = process.env.PORT || 5000; // Use logical OR operator
 
-app.use('/graphql', graphqlHTTP({
-    schema : buildSchema(`
+const app = express();
+const events = [];
 
-    type Event {
-        _id : String
-    }
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String
+      }
 
-     type RootQuery {
-        events : [String!]!
-     }
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
 
-     type RootMuatation {
-        createEvent(name:String) : String
-     }
-     schema  {
-        query : RootQuery
-        mutation : RootMuatation
-     }
+      type RootQuery {
+        events: [Event!]!
+      }
+
+      type RootMutation {
+        createEvent(eventInput: EventInput): Event
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
     `),
-    rootValue : {
-        events: () => {
-            return ['Romantic ', 'Cooking', 'All-night Cooding']
-        },
-        createEvent : (args) => {
-            const eventName = args.name 
-            return eventName
-        }
+    rootValue: {
+      events: () => {
+        return events; // Return the actual events array
+      },
+      createEvent: ({ eventInput }) => {
+        const event = {
+          _id: Math.random().toString(), // Correct the method call
+          title: eventInput.title, // Destructure the eventInput
+          description: eventInput.description,
+          price: +eventInput.price,
+          date: new Date().toISOString(),
+        };
+        events.push(event);
+        return event;
+      },
     },
-    graphiql : true
-
-}))
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
-    console.log(`The server is Listing at ${'http://localhost:5000/'.cyan.underline.bold}`);
-})
+  console.log(`The server is listening at ${`http://localhost:${PORT}/`.cyan.underline.bold}`);
+});
